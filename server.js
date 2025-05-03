24caab25143be939c723c78a1679cdc2be55b96c
ReyesMartin-BSIT32E1 <martintristanmreyes@gmail.com>
2025-05-03 22:25:22 +0800

    Added Login and Report

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2');
+const path = require('path');
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -10,9 +13,47 @@ const db = mysql.createConnection({
   database: 'newdb'
 });
 
+function getCookies(req) {
+  const raw = req.headers.cookie || '';
+  return Object.fromEntries(
+    raw.split('; ').map(cookie => {
+      const [key, value] = cookie.split('=');
+      return [key, value];
+    }).filter(Boolean)
+  );
+}
+
+function checkAuth(req, res, next) {
+  const cookies = getCookies(req);
+  if (cookies.loggedIn === 'true') {
+    next();
+  } else {
+    res.sendFile(path.join(__dirname, 'src', 'Login.html'));
+  }
+}
+
+app.get('/', (req, res) => {
+  const cookies = getCookies(req);
+  if (cookies.loggedIn === 'true') {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+  } else {
+    res.sendFile(path.join(__dirname, 'src', 'Login.html'));
+  }
+});
+
+app.post('/login', (req, res) => {
+  res.setHeader('Set-Cookie', 'loggedIn=true; Path=/; HttpOnly');
+  res.redirect('/');
+});
+
+app.get('/logout', (req, res) => {
+  res.setHeader('Set-Cookie', 'loggedIn=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
+  res.redirect('/');
+});
+
 app.use(express.static('src'));
 
-app.get('/inventory/product', (req, res) => {
+app.get('/inventory/product', checkAuth, (req, res) => {
   const query = `
     SELECT 
       p.product_number,
@@ -41,8 +82,8 @@ app.get('/inventory/product', (req, res) => {
   });
 });
 
-app.get('/inventory/product-log', (req, res) => {
-  const query = `SELECT * from product_quantity_log`;
+app.get('/inventory/product-log', checkAuth, (req, res) => {
+  const query = `SELECT * FROM product_quantity_log`;
   db.query(query, (err, results) => {
     if (err) {
       res.status(500).send('Database query error');
